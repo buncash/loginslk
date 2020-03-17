7ec98c040b2f3c89c630161580ec411a47bb3c50
buncash <61774144+buncash@users.noreply.github.com>
2020-03-17 18:12:24 +0530

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+
+import { Routes, RouterModule } from '@angular/router';
+
+import { UserSignupComponent } from './user-signup/user-signup.component';
+
+import { UserLoginComponent } from './user-login/user-login.component';
+
+import { HomeComponent } from './home/home.component';
+
+
+
+const routes: Routes = [
+
+
+
+{ path: '', redirectTo: 'user-signup', pathMatch: 'full'},
+
+{ path: 'users', component: UserSignupComponent},
+
+{ path: 'login', component: UserLoginComponent},
+
+{ path: 'home', component: HomeComponent},
+
+
+
+
+
+];
+
+
+
+@NgModule({
+
+  imports: [RouterModule.forRoot(routes)],
+
+  exports: [RouterModule]
+
+})
+
+export class AppRoutingModule { }
---
+++ minilogin2/src/main/java/com/example/minilogin/model/ProductInfo.java
@@ -0,0 +1,5 @@
+package com.example.minilogin.model;
+
+public class ProductInfo {
+
+}
